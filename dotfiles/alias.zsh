720a3af70fe67041f66a67165ccf6d6b27888ba3
Peter Goes <petergoes@gmail.com>
2022-02-15 09:46:15 +0100

    Add multiple-dot-aliases

--- dotfiles/alias.zsh
+++ dotfiles/alias.zsh
@@ -4,3 +4,8 @@ alias https='http --default-scheme=https'
 alias github='gh'
 alias showHiddenFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideHiddenFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
